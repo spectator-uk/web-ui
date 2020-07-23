42b6767d8ccc76898744bae3382337e447a55936
Ed Stephens <estephens@pressholdings.com>
2020-07-23 11:42:32 +0100

    Enhance UiIcon

--- src/components/UiIcon/index.tsx
+++ src/components/UiIcon/index.tsx
@@ -1,12 +1,14 @@
 import React, { forwardRef } from 'react'
 import { ThemeColors, ThemeSizes } from 'spectator-uk-theme'
-import { StandardProps } from '../../types/standardProps'
 import { theme } from '../../global/theme'
-import { icons } from './icons'
+import { StandardProps } from '../../types/standardProps'
 import { iconMetrics } from './metrics'
 
 export interface UiIconProps extends StandardProps {
-  icon: keyof typeof icons | undefined
+  icon: {
+    viewBox: string
+    path: string
+  }
   size?: ThemeSizes
   color?: 'currentColor' | keyof ThemeColors
 }
@@ -18,21 +20,21 @@ export const UiIcon: React.FC<UiIconProps> = forwardRef(
     const colorValue =
       color === 'currentColor' ? 'currentColor' : theme.color[color]
 
-    return (
-      <svg
-        ref={ref as any}
-        width={sizeValue}
-        height={sizeValue}
-        viewBox={icon ? icons[icon].viewBox : undefined}
-        id={id}
-        className={className}
-      >
-        <path
-          d={icon ? icons[icon]?.path : undefined}
-          fill={colorValue}
-          fillRule='nonzero'
-        />
-      </svg>
-    )
+    if (icon.viewBox && icon.path) {
+      return (
+        <svg
+          ref={ref as any}
+          width={sizeValue}
+          height={sizeValue}
+          viewBox={icon.viewBox}
+          id={id}
+          className={className}
+        >
+          <path d={icon.path} fill={colorValue} fillRule='nonzero' />
+        </svg>
+      )
+    }
+
+    return null
   }
 )
